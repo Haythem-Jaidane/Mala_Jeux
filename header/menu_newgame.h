da8a0ba082ddb057b63de206496e576e1abf6209
haythem-cpu <haythemjaidane11@gmail.com>
2021-02-28 12:41:57 +0100

    menu_newgame

---
+++ header/menu_newgame.h
@@ -0,0 +1,14 @@
+#ifndef MENU_NEWGAME
+#define MENU_NEWGAME
+
+void affichage();
+
+void affichage_button();
+
+void single_player();
+
+void multiplayer();
+
+void menu_newplayer();
+
+#endif
---
+++ source/menu_newgame.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+void affichage(){
+
+}
+
+void affichage_button(){
+
+}
+
+void single_player(){
+
+}
+
+void multiplayer(){
+
+}
+
+void menu_newgame(){
+
+}
