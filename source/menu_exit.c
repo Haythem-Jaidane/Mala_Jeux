1fc24a5593d5a7b77b9e758257d58df664baae7e
haythem-cpu <haythemjaidane11@gmail.com>
2021-02-28 14:02:48 +0100

    menu_exit

--- source/menu_exit.c
+++ source/menu_exit.c
@@ -4,5 +4,30 @@
 void affiche(){
 }
 
-void menu_exit(){
+void menu_exit(int *continuer){
+   
+   SDL_Surface *background=NULL, *yes=NULL, *no=NULL; 
+   SDL_Event event;
+   int s=1;
+   
+   /*Load_image
+   
+   */
+   while(s){
+   	SDL_PollEvent(&event);
+   	switch(event.type){
+   	    case SDL_QUIT:
+   	        s=0;
+   	        *continuer=0;
+   	        break;
+   	    case SDL_KEYDOWN:
+   	        break;
+   	    case SDL_MOUSEBUTTONDOWN:
+   	        break;
+   	    case SDL_MOUSEMOTION:
+   	        break;
+   	    
+   	}
+   }
+   
 }
