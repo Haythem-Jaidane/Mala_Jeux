2c8c3a699b0e29f28570624af53a7b5be7b1f7a1
haythem-cpu <haythemjaidane11@gmail.com>
2021-02-28 12:39:40 +0100

    menu_option

---
+++ header/menu_option.h
@@ -0,0 +1,14 @@
+#ifndef MENU_OPTION
+#define MENU_OPTION
+
+void affiche();
+
+void modifier_son();
+
+void full_screen();
+
+void simple_screen();
+
+void menu_option();
+
+#endif
---
+++ source/menu_option.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+void affiche(){
+}
+
+void modifier_son(){
+}
+
+void full_screen(){
+}
+
+void simple_screen(){
+}
+
+void menu_option(){
+}
